9081348ba98bcccec7584bfcd94ce5ff5cf91596
CharmsGraker <charmsgraker@gmail.com>
2021-08-14 18:44:15 +0800

    deploy 0.0.1

--- app/__init__.py
+++ app/__init__.py
@@ -62,12 +62,12 @@ def create_app(config_name):
     from .main import main as main_blueprint
     from .users import users as user_blueprint
     from .auth import auth as auth_blueprint
-    from .profile import  profile as profile_blueprint
+    # from .profile import  profile as profile_blueprint
 
     app.register_blueprint(main_blueprint, url_prefix='/')
     app.register_blueprint(user_blueprint, url_prefix='/users')
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    app.register_blueprint(profile_blueprint,url_prefix='/profile')
+    # app.register_blueprint(profile_blueprint,url_prefix='/profile')
 
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
